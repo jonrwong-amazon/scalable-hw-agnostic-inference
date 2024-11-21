5d0d5c930a86ce885d6f87d0a6ef9158b5c4e552
Jonathan Wong <jonrwong@amazon.com>
2024-11-20 19:38:56 -0800

    Removed bitsandbytes

--- app/run-llama.sh
+++ app/run-llama.sh
@@ -12,8 +12,7 @@ if [ "$(uname -i)" = "x86_64" ]; then
     pip install nvitop bitsandbytes accelerate protobuf --no-cache-dir transformers sentencepiece
   fi
 elif [ "$DEVICE" == "cpu" ]; then
-    python3 -m pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 diffusers transformers accelerate protobuf --no-cache-dir transformers sentencepiece bitsandbytes
-    pip install transformers accelerate bitsandbytes>0.37.0
+    python3 -m pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 diffusers transformers accelerate protobuf --no-cache-dir transformers sentencepiece
 # elif [ "$(uname -i)" = "aarch64" ]; then
 #   if [ "$DEVICE" == "cpu" ]; then
 #     python3 -m pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 diffusers transformers accelerate protobuf --no-cache-dir transformers sentencepiece
